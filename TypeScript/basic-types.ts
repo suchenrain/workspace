b42fda573fa169de90c724aa24b07e9cdb8cc0b6
suchenxiaoyu <suchenxiaoyu@gmail.com>
2017-11-07 19:19:33 +0800

    basic-types

---
+++ TypeScript/basic-types.ts
@@ -0,0 +1,21 @@
+// boolean
+let isDone:boolean=true;
+
+// number
+let decimal:number=1.1212;
+let hex:number=0x12;
+let octal:number=0o23;
+let binary:number=0b1001;
+
+// string
+let color:string="red";
+color='green';
+
+let fullname:string="Tom Cat";
+let age:number=6;
+let sentence:string=`Hello, my name is ${fullname}.
+
+i'll be ${age} years old next month`;
+//equivalent
+let sentences:string='Hello,my name is'+fullname+'.\n\n'+
+'i\'ll be '+age+' years old next month';
