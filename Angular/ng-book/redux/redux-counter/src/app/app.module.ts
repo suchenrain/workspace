fe94d0de97117f954c2d5c4e5ced63132421fc5a
suchenxiaoyu <suchenxiaoyu@gmail.com>
2018-03-26 19:21:37 +0800

    redux

--- Angular/ng-book/redux/redux-counter/src/app/app.module.ts
+++ Angular/ng-book/redux/redux-counter/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { appStoreProviders } from "./app.store";
 import { AppComponent } from './app.component';
 
 
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [appStoreProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
