770dfc40588b616b731ea848e371eb71a75d042d
suchenxiaoyu <suchenxiaoyu@gmail.com>
2018-04-04 19:12:19 +0800

    continue

--- Angular/zchen/src/lib/ui-widget/smart-image/smart-image.component.ts
+++ Angular/zchen/src/lib/ui-widget/smart-image/smart-image.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  Renderer2
+} from "@angular/core";
 
 @Component({
   selector: "zc-smart-image",
@@ -8,10 +15,26 @@ import { Component, OnInit, Input } from "@angular/core";
 export class SmartImageComponent implements OnInit {
   @Input() src: string;
   @Input() tinySrc: string;
-
-  constructor() {}
+  @ViewChild("container") placeholder: ElementRef;
+  @ViewChild("tiny") tiny: ElementRef;
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
-    let img
+    console.log(this.src);
+  }
+
+  ngAfterViewInit() {
+    this.renderer.listen(this.tiny.nativeElement, "load", () => {
+      this.renderer.addClass(this.tiny.nativeElement, "loaded");
+    });
+
+    const img = this.renderer.createElement("img");
+    this.renderer.appendChild(this.placeholder.nativeElement, img);
+    this.renderer.setAttribute(img, "src", this.src);
+
+    // console.log(img);
+    this.renderer.listen(img, "load", () => {
+      this.renderer.addClass(img, "loaded");
+    });
   }
 }
